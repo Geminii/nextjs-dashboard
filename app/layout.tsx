351061a51f5a6457703acd410e8cc4611689d323
Jimmy Jouanne <jimmy.jouanne@jobteaser.com>
2023-10-28 14:16:12 +0200

    feat: use vercel geist sans font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,12 @@
+import { GeistSans } from "geist/font";
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="en" className={GeistSans.className}>
       <body>{children}</body>
     </html>
   );
